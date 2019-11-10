8cd41a0b14dac4e5b87733a0de2f47e9e69859d5
charmockridge <charmockridge@users.noreply.github.com>
2019-11-10 18:52:18 +0000

    file update

--- start.py
+++ start.py
@@ -3,21 +3,16 @@ import sys
 import os
 
 
-path = "/Users/malcolmherbert/Documents/Charlie/Projects/MyProjects"
+path = "/Users/malcolmherbert/Documents/Charlie/Projects/MyProjects/"
 username = "your github username"
 password = "your github password"
 
+projectName = str(sys.argv[1])
+os.makedirs(path + projectName)
 
-def start():
-    projectName = str(sys.argv[1])
-    os.makedirs(path + str(projectName))
+user = Github(username, password).get_user()
+user.create_repo(str(projectName))
 
-    user = Github(username, password).get_user()
-    repo = user.create_repo(str(projectName))
-    print("\n======================================")
-    print("Repository has been successfully made!")
-    print("======================================\n")
-
-
-if __name__ = '__main__':
-    start()
+print("\n======================================")
+print("Repository has been successfully made!")
+print("======================================\n")
